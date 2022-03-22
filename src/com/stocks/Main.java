44def1c0b303aa9f4efe93287b7b4d89cf486cfe
Rehan Shekh <rehanrshekh@gmail.com>
2022-03-22 18:25:41 +0530

    Added Hashmap

--- src/com/stocks/Main.java
+++ src/com/stocks/Main.java
@@ -1,10 +1,24 @@
 package com.stocks;
 
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Welcome to Stock Maintenance Program");
         StockPortfolio value = new StockPortfolio();
         value.stockValue();
+        int i = 0;
+        while (i < 1) {
+            System.out.println("Press 1 to create another account/Press 0 to exit");
+            Scanner in = new Scanner(System.in);
+            if (in.nextInt() == 1) {
+                value.updateA();
+            } else if (in.nextInt() == 0) {
+                System.out.println("Thank You");
+                i = 1;
+            }
+        }
     }
 }
--- src/com/stocks/StockPortfolio.java
+++ src/com/stocks/StockPortfolio.java
@@ -1,28 +1,77 @@
 package com.stocks;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
-public class StockPortfolio{
+public class StockPortfolio {
     public static float total = 0;
-    public void stockValue(){
-    System.out.println("Enter No. of Stocks");
+    Map<String, List<Stock>> map = new LinkedHashMap<>();
     Scanner in = new Scanner(System.in);
-    int quantity =in.nextInt();
-    ArrayList<Stock> list = new ArrayList<>();
-        for (int i=1; i<=quantity; i++){
-        System.out.println("Enter the "+i+" share name");
-        String shareName = in.next();
-        System.out.println("Enter the no. of shares");
-        int noOfShare = in.nextInt();
-        System.out.println("Enter share price of each stock");
-        float sharePrice = in.nextFloat();
-        Stock stock = new Stock(shareName,noOfShare,sharePrice);
-        list.add(stock);
-    }for (Stock info:list){
-        System.out.println("Stock Name:"+info.getShareName()+"  Number of Shares:"+info.getNoOfShare()+"  Share Price:"+info.getSharePrice()+"  Stock Value:"+info.getNoOfShare()*info.getSharePrice());
-        total =total + (info.getNoOfShare()*info.getSharePrice());
+
+    public void stockValue() {
+
+        System.out.println("How many accounts do you want to create");
+        int accounts = in.nextInt();
+        for (int a = 1; a <= accounts; a++) {
+            System.out.println("Create an account by Name");
+            String accountName = in.next();
+            map.put(accountName, new LinkedList<>());
+            System.out.println("Enter No. of Stocks");
+            int quantity = in.nextInt();
+            for (int i = 1; i <= quantity; i++) {
+                System.out.println("Enter the " + i + " share name");
+                String shareName = in.next();
+                System.out.println("Enter the no. of shares");
+                int noOfShare = in.nextInt();
+                System.out.println("Enter share price of each stock");
+                float sharePrice = in.nextFloat();
+                Stock stock = new Stock(shareName, noOfShare, sharePrice);
+                map.get(accountName).add(stock);
+            }
+        }
+        display();
     }
-        System.out.println("The total value of all stocks is "+total);
-}
+
+    public void display() {
+        for (String key : map.keySet()) {
+            System.out.println("Account Name:" + key);
+            total = 0;
+            for (Stock info : map.get(key)) {
+                System.out.println("Stock Name:" + info.getShareName() + "  Number of Shares:" + info.getNoOfShare() + "  Share Price:" + info.getSharePrice() + "  Stock Value:" + info.getNoOfShare() * info.getSharePrice());
+                total = total + (info.getNoOfShare() * info.getSharePrice());
+            }
+            System.out.println("The total value of all stocks is " + total);
+        }
+    }
+
+    public void updateA() {
+        System.out.println("How many accounts do you want to create");
+        int accounts = in.nextInt();
+        for (int a = 1; a <= accounts; a++) {
+            int r = 1;
+            while (r > 0) {
+                System.out.println("Create an account by Name");
+                String accountName = in.next();
+                boolean key = map.containsKey(accountName);
+                if (!key) {
+                    map.put(accountName, new LinkedList<>());
+                    System.out.println("Enter No. of Stocks");
+                    int quantity = in.nextInt();
+                    for (int i = 1; i <= quantity; i++) {
+                        System.out.println("Enter the " + i + " share name");
+                        String shareName = in.next();
+                        System.out.println("Enter the no. of shares");
+                        int noOfShare = in.nextInt();
+                        System.out.println("Enter share price of each stock");
+                        float sharePrice = in.nextFloat();
+                        Stock stock = new Stock(shareName, noOfShare, sharePrice);
+                        map.get(accountName).add(stock);
+                    }
+                    display();
+                    r = 0;
+                } else {
+                    System.out.println("Account Exists, please enter another Account Name");
+                }
+            }
+        }
+    }
 }
